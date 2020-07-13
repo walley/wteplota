98785b233be49f940fdd27c037da06bbdf41fc76
walley <walley@walley.org>
2020-07-13 14:03:50 +0200

    livedata 2

--- app/src/main/java/org/walley/wteplota/wt_viewmodel.java
+++ app/src/main/java/org/walley/wteplota/wt_viewmodel.java
@@ -1,29 +1,33 @@
 package org.walley.wteplota;
 
+import android.app.Application;
+
 import com.google.gson.JsonObject;
 
 import java.util.Hashtable;
 
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
 
-public class wt_viewmodel extends ViewModel
+
+public class wt_viewmodel extends AndroidViewModel
 {
 
   private MutableLiveData<Hashtable<String, JsonObject>> server_data;
 
-  public wt_viewmodel()
+  public wt_viewmodel(Application app)
   {
+    super(app);
     server_data = new MutableLiveData<Hashtable<String, JsonObject>>();
   }
 
-  public MutableLiveData<Hashtable<String, JsonObject>> get_server_data()
+  public MutableLiveData<Hashtable<String, JsonObject>> xget_server_data()
   {
     return server_data;
   }
 
-  public LiveData<Hashtable<String, JsonObject>> getText()
+  public LiveData<Hashtable<String, JsonObject>> get_server_data()
   {
     return server_data;
   }
