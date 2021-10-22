796f78c640eac616e26b9ec5af82991d63c86093
walley <walley@walley.org>
2021-10-22 09:20:07 +0200

    preferences fragment
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_f_preferences.kt
+++ app/src/main/java/org/walley/wteplota/wt_f_preferences.kt
@@ -5,8 +5,13 @@ import androidx.preference.PreferenceFragmentCompat
 
 class wt_f_preferences : PreferenceFragmentCompat() {
   private val TAG = "WT_FP"
-  override fun onCreatePreferences(savedInstanceState: Bundle, rootKey: String) {
+
+  override fun onCreatePreferences(
+    savedInstanceState: Bundle?,
+    rootKey: String?,
+                                  ) {
     setPreferencesFromResource(R.xml.preferences, rootKey)
+//    setPreferencesFromResource(R.xml.preferences, "PreferenceScreen")
   }
 
   //https://developer.android.com/guide/topics/ui/settings.html
