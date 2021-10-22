28f8e32cfb720ea98d47f20e6ca9ca697181d7ab
walley <walley@walley.org>
2021-10-22 15:16:04 +0200

    login
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_f_login.kt
+++ app/src/main/java/org/walley/wteplota/wt_f_login.kt
@@ -1,21 +1,36 @@
 package org.walley.wteplota
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_prefs.*
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class wt_f_login : Fragment() {
+  val TAG = "WT-L"
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    val root = inflater.inflate(R.layout.fragment_prefs, container, false)
 
-    button?.setOnClickListener {
+    Log.d(TAG, "onCreateView(): start")
+
+    val root = inflater.inflate(R.layout.fragment_login, container, false)
+
+    button_login?.setOnClickListener {
+      Log.d(TAG, "klyk")
       Toast.makeText(activity, "You clicked me.", Toast.LENGTH_SHORT).show()
     }
+
+//val button = findViewById<Button>(R.id.Button)
+
+    /*val intent = Intent(this, wt_login::class.java).apply
+    {
+        //putExtra(EXTRA_MESSAGE, message)
+    }
+    startActivity(intent)
+*/
 
     return root
   }
