d472c8b66ca218d5c8437bc8471ef3cbaf688dcf
walley <walley@walley.org>
2021-10-22 15:31:45 +0200

    login it clicks
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_f_login.kt
+++ app/src/main/java/org/walley/wteplota/wt_f_login.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_login.*
@@ -12,26 +13,22 @@ import kotlinx.android.synthetic.main.fragment_login.*
 class wt_f_login : Fragment() {
   val TAG = "WT-L"
 
-  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+  private lateinit var button: Button
 
-    Log.d(TAG, "onCreateView(): start")
-
-    val root = inflater.inflate(R.layout.fragment_login, container, false)
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
 
     button_login?.setOnClickListener {
       Log.d(TAG, "klyk")
       Toast.makeText(activity, "You clicked me.", Toast.LENGTH_SHORT).show()
     }
 
-//val button = findViewById<Button>(R.id.Button)
+  }
 
-    /*val intent = Intent(this, wt_login::class.java).apply
-    {
-        //putExtra(EXTRA_MESSAGE, message)
-    }
-    startActivity(intent)
-*/
 
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    Log.d(TAG, "onCreateView(): start")
+    val root = inflater.inflate(R.layout.fragment_login, container, false)
     return root
   }
 }
