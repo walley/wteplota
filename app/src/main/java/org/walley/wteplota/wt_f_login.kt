a813a8de35803b03e2c9b1d50f6916306d39cd76
walley <walley@walley.org>
2023-07-26 15:33:39 +0200

    oauth2 login
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_f_login.kt
+++ app/src/main/java/org/walley/wteplota/wt_f_login.kt
@@ -1,5 +1,6 @@
 package org.walley.wteplota
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,18 +14,30 @@ class wt_f_login : Fragment() {
   val TAG = "WT-L"
 
   private lateinit var button_login: Button
+  private lateinit var button2_login: Button
   private lateinit var button_nextcloud: Button
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
 
     button_login.setOnClickListener {
-      Log.d(TAG, "klyk")
-      Toast.makeText(activity, "You clicked me.", Toast.LENGTH_SHORT).show()
+      Log.d(TAG, "klyk 1")
+      Toast.makeText(activity, "You clicked one.", Toast.LENGTH_SHORT).show()
+    }
+    button2_login.setOnClickListener {
+      Log.d(TAG, "klyk 2")
+      Toast.makeText(activity, "dvojka two.", Toast.LENGTH_SHORT).show()
     }
     button_nextcloud.setOnClickListener {
-      Log.d(TAG, "klyk")
+      Log.d(TAG, "klyk nextcloud")
       Toast.makeText(activity, "nextcloud.", Toast.LENGTH_SHORT).show()
+
+      val i = Intent(activity, wt_webview::class.java)
+      i.putExtra("view_id", "x");
+//      Log.d("WC",class_name + "onitemclick: view id " + view_id);
+
+      startActivity(i)
+
     }
   }
 
@@ -37,6 +50,7 @@ class wt_f_login : Fragment() {
     Log.d(TAG, "onCreateView(): start")
     val root = inflater.inflate(R.layout.fragment_login, container, false)
     button_login = root.findViewById(R.id.button_login)
+    button2_login = root.findViewById(R.id.button2_login)
     button_nextcloud = root.findViewById(R.id.button_nextcloud)
     return root
   }
--- app/src/main/java/org/walley/wteplota/wt_webview.kt
+++ app/src/main/java/org/walley/wteplota/wt_webview.kt
@@ -1,12 +1,47 @@
 package org.walley.wteplota
 
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class wt_webview : AppCompatActivity() {
+  private lateinit var wv: WebView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_webview);
+    wv = findViewById(R.id.webview)
+    wv.settings.setJavaScriptEnabled(true)
+
+    wv.webViewClient = object : WebViewClient() {
+      override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+        if (url != null) {
+          view?.loadUrl(url)
+        }
+        return true
+      }
+    }
+
+    var bundle: Bundle? = intent.extras
+    var message = bundle!!.getString("value")
+    Log.d("WC", "wt_webview onCreate(): " + message);
+
+//    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+    //var client_id = getConfigString("client_id")
+
+    var client_id = "Jjd2YSV1KK9zktD9gaDbHInooLN8QsdvwwKilvwKf4DNcRpc4HeWrsbYg1al8hw7"
+
+    var uri_redirect: String
+    uri_redirect = "https://cloud.grezl.eu/index.php/apps/oauth2/authorize?";
+    uri_redirect += "response_type=code&";
+    uri_redirect += "client_id=$client_id&";
+    uri_redirect += "state=yo&";
+    uri_redirect += "redirect_uri=https://wiot.cz/wiot/v1/oknextcloud";
+
+    wv.loadUrl(uri_redirect)
+
   }
 }
