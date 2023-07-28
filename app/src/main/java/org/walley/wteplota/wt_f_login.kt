0f3a3007e9db2f7dbc71f3bc85e42670fce5ecc1
walley <walley@walley.org>
2023-07-28 13:56:23 +0200

    oauth2 cookie
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_f_login.kt
+++ app/src/main/java/org/walley/wteplota/wt_f_login.kt
@@ -6,34 +6,60 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.CookieManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.koushikdutta.async.future.FutureCallback
+import com.koushikdutta.ion.Ion
+
 
 class wt_f_login : Fragment() {
-  val TAG = "WT-L"
+  val TAG = "WT-F-L"
 
   private lateinit var button_login: Button
   private lateinit var button2_login: Button
   private lateinit var button_nextcloud: Button
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    var url: String
+
     super.onViewCreated(view, savedInstanceState)
 
     button_login.setOnClickListener {
       Log.d(TAG, "klyk 1")
       Toast.makeText(activity, "You clicked one.", Toast.LENGTH_SHORT).show()
     }
+
     button2_login.setOnClickListener {
       Log.d(TAG, "klyk 2")
       Toast.makeText(activity, "dvojka two.", Toast.LENGTH_SHORT).show()
+
+      url = "https://wiot.cz/wiot/v1/username"
+      val data =
+        Ion.with(context).load("https://wiot.cz").setLogging("WT-F-L", Log.VERBOSE).asString().get()
+
+      Ion.with(context).load(url).asString()
+        .setCallback(FutureCallback<String?> { exception, result ->
+          if (result == null) {
+            Log.e(TAG, "klyk2: error")
+            return@FutureCallback
+          }
+          Log.d(TAG, "Klyk2: " + result.toString())
+          val cookies: String = CookieManager.getInstance().getCookie(url)
+          Log.d(TAG, "Klyk2: " + cookies)
+
+        })
+
     }
+
     button_nextcloud.setOnClickListener {
       Log.d(TAG, "klyk nextcloud")
       Toast.makeText(activity, "nextcloud.", Toast.LENGTH_SHORT).show()
 
       val i = Intent(activity, wt_webview::class.java)
-      i.putExtra("view_id", "x");
+      i.putExtra("message", "x");
 //      Log.d("WC",class_name + "onitemclick: view id " + view_id);
 
       startActivity(i)
@@ -54,15 +80,4 @@ class wt_f_login : Fragment() {
     button_nextcloud = root.findViewById(R.id.button_nextcloud)
     return root
   }
-
-  /*
-    my $uri_redirect = "https://cloud.grezl.eu/index.php/apps/oauth2/authorize?";
-  $uri_redirect .= "response_type=code&";
-  $uri_redirect .= "client_id=$client_id&";
-  $uri_redirect .= "state=yo&";
-  $uri_redirect .= "redirect_uri=https://wiot.cz/wiot/v1/oknextcloud";
-
-
-
-   */
 }
--- app/src/main/java/org/walley/wteplota/wt_webview.kt
+++ app/src/main/java/org/walley/wteplota/wt_webview.kt
@@ -1,19 +1,24 @@
 package org.walley.wteplota
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.webkit.CookieManager
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 class wt_webview : AppCompatActivity() {
   private lateinit var wv: WebView
+  private val TAG: String = "WT_WV";
 
+  @SuppressLint("SetJavaScriptEnabled")
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_webview);
     wv = findViewById(R.id.webview)
-    wv.settings.setJavaScriptEnabled(true)
+    wv.settings.javaScriptEnabled = true
+    wv.settings.domStorageEnabled = true;
 
     wv.webViewClient = object : WebViewClient() {
       override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
@@ -22,17 +27,21 @@ class wt_webview : AppCompatActivity() {
         }
         return true
       }
+
+      override fun onPageFinished(view: WebView, url: String) {
+        Log.d(TAG, "onPageFinished(): done ");
+        val cookies: String = CookieManager.getInstance().getCookie(url)
+        Log.d(TAG, "onPageFinished(): " + cookies)
+      }
     }
 
     var bundle: Bundle? = intent.extras
-    var message = bundle!!.getString("value")
-    Log.d("WC", "wt_webview onCreate(): " + message);
-
-//    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    var message = bundle!!.getString("message")
+    Log.d(TAG, "wt_webview onCreate(): bundle " + message);
 
     //var client_id = getConfigString("client_id")
-
-    var client_id = "Jjd2YSV1KK9zktD9gaDbHInooLN8QsdvwwKilvwKf4DNcRpc4HeWrsbYg1al8hw7"
+    //var client_id = "Jjd2YSV1KK9zktD9gaDbHInooLN8QsdvwwKilvwKf4DNcRpc4HeWrsbYg1al8hw7"
+    val client_id = "q19BJMSducGS6Msosga9reb5r3BaWL1DOdaT116hwtEYTAMYolpsOsSFyDRvzBZn"
 
     var uri_redirect: String
     uri_redirect = "https://cloud.grezl.eu/index.php/apps/oauth2/authorize?";
