fc3fdf351228809e05fd5b554d5a51e1fa98abf5
walley <walley@walley.org>
2023-08-01 14:52:13 +0200

    login works
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_f_login.kt
+++ app/src/main/java/org/walley/wteplota/wt_f_login.kt
@@ -6,12 +6,13 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.CookieManager
 import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.koushikdutta.async.future.FutureCallback
+import com.koushikdutta.async.http.Headers
 import com.koushikdutta.ion.Ion
+import java.net.URI
 
 
 class wt_f_login : Fragment() {
@@ -30,6 +31,8 @@ class wt_f_login : Fragment() {
     button_login.setOnClickListener {
       Log.d(TAG, "klyk 1")
       Toast.makeText(activity, "You clicked one.", Toast.LENGTH_SHORT).show()
+      val x = get_username()
+      Log.d(TAG, "klik1: $x")
     }
 
     button2_login.setOnClickListener {
@@ -37,8 +40,33 @@ class wt_f_login : Fragment() {
       Toast.makeText(activity, "dvojka two.", Toast.LENGTH_SHORT).show()
 
       url = "https://wiot.cz/wiot/v1/username"
-      val data =
-        Ion.with(context).load("https://wiot.cz").setLogging("WT-F-L", Log.VERBOSE).asString().get()
+
+      /*    val cookie_manager = java.net.CookieManager()
+          java.net.CookieHandler.setDefault(cookie_manager);
+          cookie_manager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+          val cookie = HttpCookie( "oauth2sessid","1N3XvssvvConH-1690877509")
+          cookie.domain = "https://wiot.cz"
+          cookie.path = "/"
+          cookie.version = 0
+          cookie_manager.getCookieStore().add(URI(url), cookie)
+
+          Log.d(TAG,"cookies: "+ cookie_manager.cookieStore.cookies.toString())
+    */
+
+      val cookies: String = android.webkit.CookieManager.getInstance().getCookie(url)
+      Log.d(TAG, "Klyk2: webkit cookies:" + cookies)
+
+      val middleware = Ion.getDefault(context).cookieMiddleware
+      val ion = Ion.getDefault(context)
+      ion.cookieMiddleware.clear()
+
+      val headers = Headers()
+      headers.set("Set-Cookie", "oauth2sessid=1N3XvssvvConH-1690877509")
+      val uri = URI.create(url)
+      middleware.put(uri, headers)
+      val data = Ion.with(context).load(url).setLogging(TAG, Log.INFO).asString().get()
+
+      Log.d(TAG, "Klyk2: data: $data")
 
       Ion.with(context).load(url).asString()
         .setCallback(FutureCallback<String?> { exception, result ->
@@ -47,9 +75,6 @@ class wt_f_login : Fragment() {
             return@FutureCallback
           }
           Log.d(TAG, "Klyk2: " + result.toString())
-          val cookies: String = CookieManager.getInstance().getCookie(url)
-          Log.d(TAG, "Klyk2: " + cookies)
-
         })
 
     }
@@ -69,9 +94,7 @@ class wt_f_login : Fragment() {
 
 
   override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
+    inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
                            ): View? {
     Log.d(TAG, "onCreateView(): start")
     val root = inflater.inflate(R.layout.fragment_login, container, false)
@@ -79,5 +102,38 @@ class wt_f_login : Fragment() {
     button2_login = root.findViewById(R.id.button2_login)
     button_nextcloud = root.findViewById(R.id.button_nextcloud)
     return root
+  }
+
+  fun get_username(): String {
+    var url: String
+    url = "https://wiot.cz/wiot/v1/username"
+    Log.d(TAG, "get_username(): start")
+
+    val cookies: String = android.webkit.CookieManager.getInstance().getCookie(url)
+    Log.d(TAG, "get_username(): webkit cookies:" + cookies)
+
+    val middleware = Ion.getDefault(context).cookieMiddleware
+    val ion = Ion.getDefault(context)
+    ion.cookieMiddleware.clear()
+
+    val headers = Headers()
+    headers.set("Set-Cookie", cookies)
+    val uri = URI.create(url)
+    middleware.put(uri, headers)
+    val data = Ion.with(context).load(url).setLogging(TAG, Log.INFO).asString().get()
+    Log.d(TAG, "get_username(): returned data: $data")
+
+    return data/*    Ion.with(context)
+      .load(url)
+      .asString()
+      .setCallback(FutureCallback<String?> { exception, result ->
+        if (result == null) {
+          Log.e(TAG, "klyk2: error")
+          return @FutureCallback
+        }
+
+        Log.d(TAG, "Klyk2: " + result.toString())
+      })
+*/
   }
 }
