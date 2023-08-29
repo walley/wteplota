4971d16879bd0f0885912910f6d02e06fbf2c32b
walley <walley@walley.org>
2023-08-29 14:43:44 +0200

    it works, shows room
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_f_start.kt
+++ app/src/main/java/org/walley/wteplota/wt_f_start.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.preference.PreferenceManager
 import androidx.recyclerview.widget.GridLayoutManager
@@ -48,9 +47,9 @@ class wt_f_start : wt_f_base(), adapter_RecyclerView.ItemClickListener {
     api = prefs.getString("api_type", "default_api").toString()
 
     wtviewmodel = ViewModelProviders.of(this).get(wt_viewmodel::class.java)
-    wtviewmodel!!._server_data.observe(viewLifecycleOwner, Observer {
+    wtviewmodel!!._server_data.observe(viewLifecycleOwner) {
       Log.i(TAG, "viewmodel observer onchanged()")
-    })
+    }
 
     data_hash = wtviewmodel!!._server_data.value
 
@@ -60,16 +59,16 @@ class wt_f_start : wt_f_base(), adapter_RecyclerView.ItemClickListener {
     binding.rvStart.layoutManager = GridLayoutManager(context, number_of_columns)
     binding.rvStart.adapter = adapter
 
-    binding.sSwipeContainer!!.setOnRefreshListener(OnRefreshListener {
+    binding.sSwipeContainer.setOnRefreshListener(OnRefreshListener {
       data_hash = wtviewmodel!!._server_data.value
-      binding.sSwipeContainer.setRefreshing(false)
+      binding.sSwipeContainer.isRefreshing = false
     })
 
     return root
   }
 
   override fun onItemClick(view: View?, position: Int) {
-    //TODO("Not yet implemented")
+    Log.i(TAG, "onItemClick($position)")
   }
 
   override fun onStart() {
@@ -107,13 +106,54 @@ class wt_f_start : wt_f_base(), adapter_RecyclerView.ItemClickListener {
     val room_data = data_hash!![room_name]
     var device: wt_device
 
+    devices_array.clear()
+
+    Log.i(TAG, "show_listview($room_name): room_data> $room_data")
+
     if (room_data != null) {
-      for ((key, value) in room_data.entrySet()) {
-        if (key != "Nazev") {
-          device = wt_device(key, value.toString())
-          add_to_devices_list(key, value.toString())
+      if (api == "walley") {
+        var device_name = ""
+        var device_desc = ""
+        var device_type = ""
+
+        for ((key, value) in room_data.entrySet()) {
+          Log.d(TAG, "show_listview($room_name): parsed $key $value")
+
+          for ((entry_key, entry_value) in value.asJsonObject.entrySet()) {
+            Log.d(TAG, "show_listview($room_name): entry parsed $entry_key $entry_value")
+            when (entry_key) {
+              "name" -> {
+                device_name = entry_value.toString()
+                Log.d(TAG, "show_listview($room_name): device name $device_name")
+              }
+
+              "type" -> {
+                device_type = entry_value.toString()
+                Log.d(TAG, "show_listview($room_name): device type $device_type")
+              }
+
+              "note" -> {
+                device_desc = entry_value.toString()
+                Log.d(TAG, "show_listview($room_name): device name $device_desc")
+              }
+            }
+          }
+          device = wt_device(device_name, device_desc, device_type)
+          devices_array.add(device)
+          Log.d(
+            TAG, "show_listview($room_name): add($device_name, $device_desc, $device_type)"
+               )
         }
-        Log.i(TAG, "added $key")
+      }
+    }
+
+    if (api == "marek") {
+      if (room_data != null) {
+        for ((key, value) in room_data.entrySet()) {
+          if (key != "Nazev") {
+            add_to_devices_list(key, value.toString())
+          }
+        }
       }
     }
     dump_devices_array()
@@ -123,51 +163,53 @@ class wt_f_start : wt_f_base(), adapter_RecyclerView.ItemClickListener {
 
   private fun add_to_devices_list(name: String, value: String) {
 
-    val device_name: String
-    val device_type: String
-    val device_value: String
+    var device_name: String
+    var device_type: String
+    var device_value: String
     var device_index = 0
     var has_name = false
     var has_type = false
     var has_value = false
 
-    if (name.contains(".typ")) {
-      device_name = name.substring(0, name.length - 4).replace("\"", "")
-      device_type = value.replace("\"", "")
-      device_value = ""
-    } else {
-      device_name = name.replace("\"", "")
-      device_type = ""
-      device_value = value.replace("\"", "")
-    }
-    for (i in devices_array.indices) {
-      val x = devices_array[i]
-      if (x.name == device_name) {
-        has_name = true
-        device_index = i
-        if (x.type != null && !x.type.isEmpty()) {
-          has_type = true
+    if (api == "marek") {
+      if (name.contains(".typ")) {
+        device_name = name.substring(0, name.length - 4).replace("\"", "")
+        device_type = value.replace("\"", "")
+        device_value = ""
+      } else {
+        device_name = name.replace("\"", "")
+        device_type = ""
+        device_value = value.replace("\"", "")
+      }
+      for (i in devices_array.indices) {
+        val x = devices_array[i]
+        if (x.name == device_name) {
+          has_name = true
+          device_index = i
+          if (x.type != null && !x.type.isEmpty()) {
+            has_type = true
+          }
+          if (x.value != null && !x.value.isEmpty()) {
+            has_value = true
+          }
+          break
         }
-        if (x.value != null && !x.value.isEmpty()) {
-          has_value = true
+      }
+      Log.i(TAG, "a_t_d_l($name,$value): has_value:$has_value, has_type:$has_type")
+      if (has_name) {
+        Log.i(TAG, "a_t_d_l($name,$value): existing :$device_name,$device_value,$device_type")
+        devices_array[device_index].name = device_name
+        if (!has_type) {
+          devices_array[device_index].type = device_type
         }
-        break
+        if (!has_value) {
+          devices_array[device_index].value = device_value
+        }
+      } else {
+        Log.i(TAG, "a_t_d_l($name,$value): new :$device_name,$device_value,$device_type")
+        val x = wt_device(device_name, device_value, device_type)
+        devices_array.add(x)
       }
-    }
-    Log.i(TAG, "a_t_d_l($name,$value): has_value:$has_value, has_type:$has_type")
-    if (has_name) {
-      Log.i(TAG, "a_t_d_l($name,$value): existing :$device_name,$device_value,$device_type")
-      devices_array[device_index].name = device_name
-      if (!has_type) {
-        devices_array[device_index].type = device_type
-      }
-      if (!has_value) {
-        devices_array[device_index].value = device_value
-      }
-    } else {
-      Log.i(TAG, "a_t_d_l($name,$value): new :$device_name,$device_value,$device_type")
-      val x = wt_device(device_name, device_value, device_type)
-      devices_array.add(x)
     }
   }
 
