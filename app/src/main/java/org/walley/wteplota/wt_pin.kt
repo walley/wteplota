b3571fdccfd9ddf8c4669e6884aef5eb6357fa9e
walley <walley@walley.org>
2024-02-01 10:24:46 +0100

    pin works kinda
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_pin.kt
+++ app/src/main/java/org/walley/wteplota/wt_pin.kt
@@ -1,6 +1,8 @@
 package org.walley.wteplota
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -14,71 +16,113 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class wt_pin : AppCompatActivity() {
 
   val pink700 = Color(0XFFD74A5B);
+  var pin: String = ""
+  val context: Context = this
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-//      Surface(color = MaterialTheme.colors.background) {
-      Surface(color = pink700) {
-        var pin by remember { mutableStateOf("") }
-        Column(
+      Screen()
+    }
+
+  }
+
+  @Composable
+  fun button_row(from: Int): String {
+
+    Row(
+      Modifier.fillMaxWidth(), Arrangement.Center
+    ) {
+      for (i in from .. from + 2) {
+        Button(
+          onClick = { pin += "$i" },
           modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+            .padding(8.dp)
+            .size(64.dp)
         ) {
-          BackHandler {
-            // Handle back button press
-          }
-          Text(
-            text = "Enter your PIN",
-//            style = MaterialTheme.typography.h5,
-            modifier = Modifier.padding(bottom = 16.dp)
-          )
-          Row(
-            Modifier.fillMaxWidth(), Arrangement.Center
+          Text(text = "$i")
+        }
+      }
+    }
+    return pin
+  }
+
+  @Preview
+  @Composable
+  fun Screen() {
+//      Surface(color = MaterialTheme.colors.background) {
+    Surface(color = pink700) {
+      Column(
+        modifier = Modifier
+          .fillMaxSize()
+          .padding(16.dp)
+      ) {
+        BackHandler {
+          // Handle back button press
+        }
+        Text(
+          text = "Enter your PIN",
+          style = typography.headlineLarge,
+          modifier = Modifier.padding(bottom = 16.dp)
+        )
+        button_row(1);
+        button_row(4);
+        button_row(7);
+        Row(
+          Modifier.fillMaxWidth(), Arrangement.Center
+        ) {
+          Button(
+            onClick = { pin = "" },
+            modifier = Modifier
+              .padding(8.dp)
+              .size(64.dp)
           ) {
-            for (i in 1 .. 9) {
-              Button(
-                onClick = { pin += i },
-                modifier = Modifier
-                  .padding(8.dp)
-                  .size(64.dp)
-              ) {
-                Text(text = "$i")
-              }
-            }
-            Button(
-              onClick = { pin += "0" },
-              modifier = Modifier
-                .padding(8.dp)
-                .size(64.dp)
-            ) {
-              Text(text = "0")
-            }
+            Text(text = "X")
           }
-          Spacer(modifier = Modifier.height(16.dp))
+          Button(
+            onClick = { pin += "0" },
+            modifier = Modifier
+              .padding(8.dp)
+              .size(64.dp)
+          ) {
+            Text(text = "0")
+          }
           Button(
             onClick = {
-              // TODO: Implement your logic to verify the entered PIN.
-            }, modifier = Modifier.fillMaxWidth()
+              pin = pin.dropLast(1)
+              //pin.substring(1, pin.length - 1)
+            },
+            modifier = Modifier
+              .padding(8.dp)
+              .size(64.dp)
           ) {
-            Text(text = "Submit")
+            Text(text = "<-")
           }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(
+          onClick = {
+            Toast.makeText(context, pin, Toast.LENGTH_SHORT)
+              .show()
+          }, modifier = Modifier.fillMaxWidth()
+        ) {
+          Text(text = "Submit")
         }
       }
     }
   }
+
 }
