683987138ce506aa8c7c9bede68b91a96655e19f
walley <walley@walley.org>
2024-02-02 12:48:29 +0100

    theme
    Signed-off-by: walley <walley@walley.org>

---
+++ app/src/main/java/org/walley/wteplota/wt_theme.kt
@@ -0,0 +1,122 @@
+package org.walley.wteplota
+
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Black
+import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.Color.Companion.White
+
+val md_theme_light_primary = Color(0xFF825500)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFFFDDB3)
+val md_theme_light_onPrimaryContainer = Color(0xFF291800)
+val md_theme_light_secondary = Color(0xFF6F5B40)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFFBDEBC)
+val md_theme_light_onSecondaryContainer = Color(0xFF271904)
+val md_theme_light_tertiary = Color(0xFF51643F)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFD4EABB)
+val md_theme_light_onTertiaryContainer = Color(0xFF102004)
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+val md_theme_light_background = Color(0xFFFFFBFF)
+val md_theme_light_onBackground = Color(0xFF1F1B16)
+val md_theme_light_surface = Color(0xFFFFFBFF)
+val md_theme_light_onSurface = Color(0xFF1F1B16)
+val md_theme_light_surfaceVariant = Color(0xFFF0E0CF)
+val md_theme_light_onSurfaceVariant = Color(0xFF4F4539)
+val md_theme_light_outline = Color(0xFF817567)
+val md_theme_light_inverseOnSurface = Color(0xFFF9EFE7)
+val md_theme_light_inverseSurface = Color(0xFF34302A)
+val md_theme_light_inversePrimary = Color(0xFFFFB951)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF825500)
+val md_theme_light_outlineVariant = Color(0xFFD3C4B4)
+val md_theme_light_scrim = Color(0xFF000000)
+
+val md_theme_dark_primary = Color(0xFFFFB951)
+val md_theme_dark_onPrimary = Color(0xFF452B00)
+val md_theme_dark_primaryContainer = Color(0xFF633F00)
+val md_theme_dark_onPrimaryContainer = Color(0xFFFFDDB3)
+val md_theme_dark_secondary = Color(0xFFDDC2A1)
+val md_theme_dark_onSecondary = Color(0xFF3E2D16)
+val md_theme_dark_secondaryContainer = Color(0xFF56442A)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFBDEBC)
+val md_theme_dark_tertiary = Color(0xFFB8CEA1)
+val md_theme_dark_onTertiary = Color(0xFF243515)
+val md_theme_dark_tertiaryContainer = Color(0xFF3A4C2A)
+val md_theme_dark_onTertiaryContainer = Color(0xFFD4EABB)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = Color(0xFF1F1B16)
+val md_theme_dark_onBackground = Color(0xFFEAE1D9)
+val md_theme_dark_surface = Color(0xFF1F1B16)
+val md_theme_dark_onSurface = Color(0xFFEAE1D9)
+val md_theme_dark_surfaceVariant = Color(0xFF4F4539)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD3C4B4)
+val md_theme_dark_outline = Color(0xFF9C8F80)
+val md_theme_dark_inverseOnSurface = Color(0xFF1F1B16)
+val md_theme_dark_inverseSurface = Color(0xFFEAE1D9)
+val md_theme_dark_inversePrimary = Color(0xFF825500)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFFFB951)
+val md_theme_dark_outlineVariant = Color(0xFF4F4539)
+val md_theme_dark_scrim = Color(0xFF000000)
+
+val red200 = Color(0XFFEF9A9A)
+val red500 = Color(0XFFF44336)
+val red700 = Color(0XFFD32F2F)
+val pink200 = Color(0XFFF27584)
+val pink500 = Color(0XFFEF5366)
+
+val pink700 = Color(0XFFD74A5B)
+val purple200 = Color(0XFFCE93D8)
+val purple500 = Color(0XFF9C27B0)
+val purple700 = Color(0XFF7B1FA2)
+
+val indigo200 = Color(0XFF9FA8DA)
+val indigo500 = Color(0XFF3F51B5)
+val indigo700 = Color(0XFF303f9f)
+val blue200 = Color(0XFF90CAF9)
+val blue500 = Color(0xFF2195F2)
+val blue700 = Color(0xFF1976D2)
+
+val teal200 = Color(0XFF80DEEA)
+
+val green200 = Color(0XFFA5D6A7)
+val green500 = Color(0XFF4CAF50)
+val green700 = Color(0XFF388E3C)
+
+val yellow200 = Color(0XFFFFF59D)
+val yellow500 = Color(0XFFFFEB3B)
+val yellow700 = Color(0XFFFBC02D)
+
+val orange200 = Color(0XFFFFCC80)
+val orange500 = Color(0XFFFF9800)
+val orange700 = Color(0XFFF57C00)
+
+val brown200 = Color(0XFFBCAAA4)
+val brown500 = Color(0XFF795548)
+val brown700 = Color(0XFF5D4037)
+val grey200 = Color(0XFFEEEEEE)
+val grey500 = Color(0XFF9E9E9E)
+val grey700 = Color(0XFF616161)
+
+private val DarkOrangeColorPalette = darkColorScheme(
+  primary = orange200,
+  secondary = orange700,
+  tertiary = teal200,
+  background = Black,
+  surface = Black,
+  onPrimary = Black,
+  onTertiary = White,
+  onBackground = White,
+  onSurface = White,
+  error = Red,
+)
+
