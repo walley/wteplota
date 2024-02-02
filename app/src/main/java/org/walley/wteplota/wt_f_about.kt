fbf011f941cf893c3650819ed13046a1424726f3
walley <walley@walley.org>
2024-02-02 12:20:15 +0100

    gradle upgrade

--- app/src/main/java/org/walley/wteplota/wt_f_about.kt
+++ app/src/main/java/org/walley/wteplota/wt_f_about.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.multidex.BuildConfig
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 
