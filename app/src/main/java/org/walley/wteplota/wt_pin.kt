7cb97e99c5f7da343e696516d34d2a50297c7a33
walley <walley@walley.org>
2024-02-05 15:49:46 +0100

    pin show
    Signed-off-by: walley <walley@walley.org>

--- app/src/main/java/org/walley/wteplota/wt_pin.kt
+++ app/src/main/java/org/walley/wteplota/wt_pin.kt
@@ -6,6 +6,9 @@ import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,8 +16,10 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Surface
@@ -22,12 +27,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.times
 
 class wt_pin : AppCompatActivity() {
 
-  val pink700 = Color(0XFFD74A5B);
+  class Size {
+    @Composable
+    fun height(): Int {
+      val configuration = LocalConfiguration.current
+      return configuration.screenHeightDp
+    }
+
+    @Composable
+    fun width(): Int {
+      val configuration = LocalConfiguration.current
+      return configuration.screenWidthDp
+    }
+  }
+
   var pin: String = ""
   val context: Context = this
 
@@ -37,6 +57,52 @@ class wt_pin : AppCompatActivity() {
       Screen()
     }
 
+  }
+
+  @Composable
+  fun MyCanvas() {
+    Canvas(modifier = Modifier.fillMaxSize()) {
+      drawRect(color = Color.Blue, size = size)
+    }
+  }
+
+  @Composable
+  fun pin_view(pin: String) {
+    val size = Size()
+    val screenHeight = size.height()
+
+    Row(
+      modifier = Modifier
+        .height(40.dp)
+        .fillMaxWidth()
+        .background(
+          Color.Red, shape = RoundedCornerShape(4.dp)
+        )
+        .border(2.dp, Color.Magenta),
+    ) {
+
+      val circleRadius = 10.dp
+      val circleSpacing = 10.dp
+      val totalWidth = (circleRadius * 2 * 4) + (circleSpacing * 3)
+      val startX = (size.width().dp - totalWidth) / 2
+//      val startX = (200.dp - totalWidth) / 2
+      repeat(4) { index ->
+        val isFilled = index < pin.length
+        val color = if (isFilled) Color.Green else Color.White
+        var cspacing = 0.dp
+        if (index != 1) {
+          cspacing = circleSpacing
+        }
+
+        val x = startX + (index * (circleRadius * 2 + circleRadius)) - 6.dp
+
+        Canvas(
+          modifier = Modifier.offset(x, circleRadius)
+        ) {
+          drawCircle(color = color, radius = circleRadius.toPx())
+        }
+      }
+    }
   }
 
   @Composable
@@ -62,8 +128,8 @@ class wt_pin : AppCompatActivity() {
   @Preview
   @Composable
   fun Screen() {
-//      Surface(color = MaterialTheme.colors.background) {
-    Surface(color = pink700) {
+    Surface(color = DarkOrangeColorPalette.background) {
+
       Column(
         modifier = Modifier
           .fillMaxSize()
@@ -73,10 +139,13 @@ class wt_pin : AppCompatActivity() {
           // Handle back button press
         }
         Text(
+          color = Color.Red,
           text = "Enter your PIN",
           style = typography.headlineLarge,
           modifier = Modifier.padding(bottom = 16.dp)
         )
+        pin_view("123")
+
         button_row(1);
         button_row(4);
         button_row(7);
@@ -84,8 +153,7 @@ class wt_pin : AppCompatActivity() {
           Modifier.fillMaxWidth(), Arrangement.Center
         ) {
           Button(
-            onClick = { pin = "" },
-            modifier = Modifier
+            onClick = { pin = "" }, modifier = Modifier
               .padding(8.dp)
               .size(64.dp)
           ) {
--- app/src/main/java/org/walley/wteplota/wt_theme.kt
+++ app/src/main/java/org/walley/wteplota/wt_theme.kt
@@ -1,72 +1,76 @@
 package org.walley.wteplota
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.Color.Companion.Red
 import androidx.compose.ui.graphics.Color.Companion.White
 
-val md_theme_light_primary = Color(0xFF825500)
-val md_theme_light_onPrimary = Color(0xFFFFFFFF)
-val md_theme_light_primaryContainer = Color(0xFFFFDDB3)
-val md_theme_light_onPrimaryContainer = Color(0xFF291800)
-val md_theme_light_secondary = Color(0xFF6F5B40)
-val md_theme_light_onSecondary = Color(0xFFFFFFFF)
-val md_theme_light_secondaryContainer = Color(0xFFFBDEBC)
-val md_theme_light_onSecondaryContainer = Color(0xFF271904)
-val md_theme_light_tertiary = Color(0xFF51643F)
-val md_theme_light_onTertiary = Color(0xFFFFFFFF)
-val md_theme_light_tertiaryContainer = Color(0xFFD4EABB)
-val md_theme_light_onTertiaryContainer = Color(0xFF102004)
-val md_theme_light_error = Color(0xFFBA1A1A)
-val md_theme_light_errorContainer = Color(0xFFFFDAD6)
-val md_theme_light_onError = Color(0xFFFFFFFF)
-val md_theme_light_onErrorContainer = Color(0xFF410002)
-val md_theme_light_background = Color(0xFFFFFBFF)
-val md_theme_light_onBackground = Color(0xFF1F1B16)
-val md_theme_light_surface = Color(0xFFFFFBFF)
-val md_theme_light_onSurface = Color(0xFF1F1B16)
-val md_theme_light_surfaceVariant = Color(0xFFF0E0CF)
-val md_theme_light_onSurfaceVariant = Color(0xFF4F4539)
-val md_theme_light_outline = Color(0xFF817567)
-val md_theme_light_inverseOnSurface = Color(0xFFF9EFE7)
-val md_theme_light_inverseSurface = Color(0xFF34302A)
-val md_theme_light_inversePrimary = Color(0xFFFFB951)
-val md_theme_light_shadow = Color(0xFF000000)
-val md_theme_light_surfaceTint = Color(0xFF825500)
-val md_theme_light_outlineVariant = Color(0xFFD3C4B4)
-val md_theme_light_scrim = Color(0xFF000000)
+val w_light_primary = Color(0xFF825500)
+val w_light_onPrimary = Color(0xFFFFFFFF)
+val w_light_primaryContainer = Color(0xFFFFDDB3)
+val w_light_onPrimaryContainer = Color(0xFF291800)
+val w_light_secondary = Color(0xFF6F5B40)
+val w_light_onSecondary = Color(0xFFFFFFFF)
+val w_light_secondaryContainer = Color(0xFFFBDEBC)
+val w_light_onSecondaryContainer = Color(0xFF271904)
+val w_light_tertiary = Color(0xFF51643F)
+val w_light_onTertiary = Color(0xFFFFFFFF)
+val w_light_tertiaryContainer = Color(0xFFD4EABB)
+val w_light_onTertiaryContainer = Color(0xFF102004)
+val w_light_error = Color(0xFFBA1A1A)
+val w_light_errorContainer = Color(0xFFFFDAD6)
+val w_light_onError = Color(0xFFFFFFFF)
+val w_light_onErrorContainer = Color(0xFF410002)
+val w_light_background = Color(0xFFFFFBFF)
+val w_light_onBackground = Color(0xFF1F1B16)
+val w_light_surface = Color(0xFFFFFBFF)
+val w_light_onSurface = Color(0xFF1F1B16)
+val w_light_surfaceVariant = Color(0xFFF0E0CF)
+val w_light_onSurfaceVariant = Color(0xFF4F4539)
+val w_light_outline = Color(0xFF817567)
+val w_light_inverseOnSurface = Color(0xFFF9EFE7)
+val w_light_inverseSurface = Color(0xFF34302A)
+val w_light_inversePrimary = Color(0xFFFFB951)
+val w_light_shadow = Color(0xFF000000)
+val w_light_surfaceTint = Color(0xFF825500)
+val w_light_outlineVariant = Color(0xFFD3C4B4)
+val w_light_scrim = Color(0xFF000000)
 
-val md_theme_dark_primary = Color(0xFFFFB951)
-val md_theme_dark_onPrimary = Color(0xFF452B00)
-val md_theme_dark_primaryContainer = Color(0xFF633F00)
-val md_theme_dark_onPrimaryContainer = Color(0xFFFFDDB3)
-val md_theme_dark_secondary = Color(0xFFDDC2A1)
-val md_theme_dark_onSecondary = Color(0xFF3E2D16)
-val md_theme_dark_secondaryContainer = Color(0xFF56442A)
-val md_theme_dark_onSecondaryContainer = Color(0xFFFBDEBC)
-val md_theme_dark_tertiary = Color(0xFFB8CEA1)
-val md_theme_dark_onTertiary = Color(0xFF243515)
-val md_theme_dark_tertiaryContainer = Color(0xFF3A4C2A)
-val md_theme_dark_onTertiaryContainer = Color(0xFFD4EABB)
-val md_theme_dark_error = Color(0xFFFFB4AB)
-val md_theme_dark_errorContainer = Color(0xFF93000A)
-val md_theme_dark_onError = Color(0xFF690005)
-val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
-val md_theme_dark_background = Color(0xFF1F1B16)
-val md_theme_dark_onBackground = Color(0xFFEAE1D9)
-val md_theme_dark_surface = Color(0xFF1F1B16)
-val md_theme_dark_onSurface = Color(0xFFEAE1D9)
-val md_theme_dark_surfaceVariant = Color(0xFF4F4539)
-val md_theme_dark_onSurfaceVariant = Color(0xFFD3C4B4)
-val md_theme_dark_outline = Color(0xFF9C8F80)
-val md_theme_dark_inverseOnSurface = Color(0xFF1F1B16)
-val md_theme_dark_inverseSurface = Color(0xFFEAE1D9)
-val md_theme_dark_inversePrimary = Color(0xFF825500)
-val md_theme_dark_shadow = Color(0xFF000000)
-val md_theme_dark_surfaceTint = Color(0xFFFFB951)
-val md_theme_dark_outlineVariant = Color(0xFF4F4539)
-val md_theme_dark_scrim = Color(0xFF000000)
+val w_dark_primary = Color(0xFFFFB951)
+val w_dark_onPrimary = Color(0xFF452B00)
+val w_dark_primaryContainer = Color(0xFF633F00)
+val w_dark_onPrimaryContainer = Color(0xFFFFDDB3)
+val w_dark_secondary = Color(0xFFDDC2A1)
+val w_dark_onSecondary = Color(0xFF3E2D16)
+val w_dark_secondaryContainer = Color(0xFF56442A)
+val w_dark_onSecondaryContainer = Color(0xFFFBDEBC)
+val w_dark_tertiary = Color(0xFFB8CEA1)
+val w_dark_onTertiary = Color(0xFF243515)
+val w_dark_tertiaryContainer = Color(0xFF3A4C2A)
+val w_dark_onTertiaryContainer = Color(0xFFD4EABB)
+val w_dark_error = Color(0xFFFFB4AB)
+val w_dark_errorContainer = Color(0xFF93000A)
+val w_dark_onError = Color(0xFF690005)
+val w_dark_onErrorContainer = Color(0xFFFFDAD6)
+val w_dark_background = Color(0xFF1F1B16)
+val w_dark_onBackground = Color(0xFFEAE1D9)
+val w_dark_surface = Color(0xFF1F1B16)
+val w_dark_onSurface = Color(0xFFEAE1D9)
+val w_dark_surfaceVariant = Color(0xFF4F4539)
+val w_dark_onSurfaceVariant = Color(0xFFD3C4B4)
+val w_dark_outline = Color(0xFF9C8F80)
+val w_dark_inverseOnSurface = Color(0xFF1F1B16)
+val w_dark_inverseSurface = Color(0xFFEAE1D9)
+val w_dark_inversePrimary = Color(0xFF825500)
+val w_dark_shadow = Color(0xFF000000)
+val w_dark_surfaceTint = Color(0xFFFFB951)
+val w_dark_outlineVariant = Color(0xFF4F4539)
+val w_dark_scrim = Color(0xFF000000)
 
 val red200 = Color(0XFFEF9A9A)
 val red500 = Color(0XFFF44336)
@@ -107,7 +111,7 @@ val grey200 = Color(0XFFEEEEEE)
 val grey500 = Color(0XFF9E9E9E)
 val grey700 = Color(0XFF616161)
 
-private val DarkOrangeColorPalette = darkColorScheme(
+public val DarkOrangeColorPalette = darkColorScheme(
   primary = orange200,
   secondary = orange700,
   tertiary = teal200,
@@ -119,4 +123,83 @@ private val DarkOrangeColorPalette = darkColorScheme(
   onSurface = White,
   error = Red,
 )
+
+private val LightColors = lightColorScheme(
+  primary = w_light_primary,
+  onPrimary = w_light_onPrimary,
+  primaryContainer = w_light_primaryContainer,
+  onPrimaryContainer = w_light_onPrimaryContainer,
+  secondary = w_light_secondary,
+  onSecondary = w_light_onSecondary,
+  secondaryContainer = w_light_secondaryContainer,
+  onSecondaryContainer = w_light_onSecondaryContainer,
+  tertiary = w_light_tertiary,
+  onTertiary = w_light_onTertiary,
+  tertiaryContainer = w_light_tertiaryContainer,
+  onTertiaryContainer = w_light_onTertiaryContainer,
+  error = w_light_error,
+  errorContainer = w_light_errorContainer,
+  onError = w_light_onError,
+  onErrorContainer = w_light_onErrorContainer,
+  background = w_light_background,
+  onBackground = w_light_onBackground,
+  surface = w_light_surface,
+  onSurface = w_light_onSurface,
+  surfaceVariant = w_light_surfaceVariant,
+  onSurfaceVariant = w_light_onSurfaceVariant,
+  outline = w_light_outline,
+  inverseOnSurface = w_light_inverseOnSurface,
+  inverseSurface = w_light_inverseSurface,
+  inversePrimary = w_light_inversePrimary,
+  surfaceTint = w_light_surfaceTint,
+  outlineVariant = w_light_outlineVariant,
+  scrim = w_light_scrim,
+)
+
+private val DarkColors = darkColorScheme(
+  primary = w_dark_primary,
+  onPrimary = w_dark_onPrimary,
+  primaryContainer = w_dark_primaryContainer,
+  onPrimaryContainer = w_dark_onPrimaryContainer,
+  secondary = w_dark_secondary,
+  onSecondary = w_dark_onSecondary,
+  secondaryContainer = w_dark_secondaryContainer,
+  onSecondaryContainer = w_dark_onSecondaryContainer,
+  tertiary = w_dark_tertiary,
+  onTertiary = w_dark_onTertiary,
+  tertiaryContainer = w_dark_tertiaryContainer,
+  onTertiaryContainer = w_dark_onTertiaryContainer,
+  error = w_dark_error,
+  errorContainer = w_dark_errorContainer,
+  onError = w_dark_onError,
+  onErrorContainer = w_dark_onErrorContainer,
+  background = w_dark_background,
+  onBackground = w_dark_onBackground,
+  surface = w_dark_surface,
+  onSurface = w_dark_onSurface,
+  surfaceVariant = w_dark_surfaceVariant,
+  onSurfaceVariant = w_dark_onSurfaceVariant,
+  outline = w_dark_outline,
+  inverseOnSurface = w_dark_inverseOnSurface,
+  inverseSurface = w_dark_inverseSurface,
+  inversePrimary = w_dark_inversePrimary,
+  surfaceTint = w_dark_surfaceTint,
+  outlineVariant = w_dark_outlineVariant,
+  scrim = w_dark_scrim,
+)
+
+@Composable
+fun AppTheme(
+  useDarkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit
+) {
+  val colors = if (!useDarkTheme) {
+    LightColors
+  } else {
+    DarkOrangeColorPalette
+  }
+
+  MaterialTheme(
+    colorScheme = colors, content = content
+  )
+}
 
